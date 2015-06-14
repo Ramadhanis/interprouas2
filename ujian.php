1bc93e2c746f70ab77754870e866e9257e666165
MegasariRamadhanis <ramadhanism@yahoo.com>
2015-06-13 19:52:42 -0700

    kasi timer/ timer dikasi berapa enak e gan? ::D

--- ujian.php
+++ ujian.php
@@ -1,4 +1,62 @@
-<div>
+<div><script src="http://code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+ 
+   $(document).ready(function() {
+      /** Membuat Waktu Mulai Hitung Mundur Dengan 
+       * var detik = 0,
+       * var menit = 1,
+       * var jam = 1
+       */
+       var detik = 0;
+       var menit = 1;
+       var jam = 0;
+ 
+      /**
+       * Membuat function hitung() sebagai Penghitungan Waktu
+       */
+       function hitung() {
+          /** setTimout(hitung, 1000) digunakan untuk 
+	   *  mengulang atau merefresh halaman selama 1000 (1 detik) */
+	   setTimeout(hitung,1000);
+ 
+	  /** Menampilkan Waktu Timer pada Tag #Timer di HTML yang tersedia */
+	   $('#timer').html( 'Sisa Waktu : ' + jam + ' Jam - ' + menit + ' Menit - ' + detik + ' Detik ');
+ 
+	  /** Melakukan Hitung Mundur dengan Mengurangi variabel detik - 1 */
+	   detik --;
+ 
+	  /** Jika var detik < 0
+	   *  var detik akan dikembalikan ke 59
+	   *  Menit akan Berkurang 1
+	   */
+	   if(detik < 0) {
+	      detik = 10;
+	      menit --;
+ 
+	      /** Jika menit < 0
+	       *  Maka menit akan dikembali ke 59
+	       *  Jam akan Berkurang 1
+	       */
+	       if(menit < 0) {
+ 		  menit = 0;
+		  jam --;
+ 
+		  /** Jika var jam < 0
+		   *  clearInterval() Memberhentikan Interval 
+		   *  Dan Halaman akan membuka http://tahukahkau.com/0
+		   */
+		   if(jam < 0) { 
+                      clearInterval();
+ 		      window.location = "http://localhost/c/ujian_online/index.php?page=jawaban";
+                   }
+	       }
+	   } 		
+        }
+ 	/** Menjalankan Function Hitung Waktu Mundur */
+        hitung();
+   });
+// ]]></script>
+
 </head>
 <body>
 <div id='timer'></div>
